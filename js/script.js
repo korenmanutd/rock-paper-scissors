8942c5018066091919a189fd6d2c92d5d0436484
korenmanutd <korenmanutd@gmail.com>
2022-05-19 21:53:12 +0300

    Add game() function
    Set up counters for score (player, computer).
    Set up a loop to play X times I'd like.
    Set up the prompt and also the call to chose random action by computer.
    See what the result is in playRound, compare and add points to winner of round.
    Check for total score, announce winner.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,70 @@
 let action = ['rock', 'paper', 'scissors'];
-const playerSelection = prompt("Please pick your hand - Rock, Paper or Scissors!").toLowerCase();
-
 function computerPlay(){
     return randomPick = action[Math.floor(Math.random() * action.length)];
 }
 
 function playRound(playerSelection, computerSelection){
+
     if(playerSelection == 'rock' && computerSelection == 'rock'){
-        return result = 'a tie';
+        return result = 'tie';
     } else if (playerSelection == 'rock' && computerSelection == 'paper'){
-        return result = 'computer wins - paper beats rock';
+        return result = 'computer';
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return result = 'player wins - rock beats scissors';
+        return result = 'player';
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return result = 'player wins - paper beats rock';
+        return result = 'player';
     } else if (playerSelection == 'paper' && computerSelection == 'paper'){
-        return result = 'a tie';
+        return result = 'tie';
     } else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        return result = 'computer wins - scissors beat paper';
+        return result = 'computer';
     } else if (playerSelection == 'scissors' && computerSelection == 'rock'){
-        return result = 'computer wins - rock beats scissors';
+        return result = 'computer';
     } else if (playerSelection == 'scissors' && computerSelection == 'paper'){
-        return result = 'player wins - scissors beat paper';
+        return result = 'player';
     } else if (playerSelection == 'scissors' && computerSelection == 'scissors'){
-        return result = 'a tie';
+        return result = 'tie';
     } else if (playerSelection != 'scissors' || playerSelection != 'paper' || playerSelection != 'rock'){
         return result = "cant do that"
     }
 }
 
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+    // set a counter for player and computer
+    let playerScore = 0;
+    let computerScore = 0;
+    //play 5 rounds
+    for(let i = 0; i < 5; i++){
+        // call the computerSelection function - computerPlay() to pick random action
+        const computerSelection = computerPlay();
+        let playerSelection = prompt("Please pick your hand - Rock, Paper or Scissors!").toLowerCase();
+        playRound(playerSelection, computerSelection)
+        // log what the computer picks so it looks as real as it can
+        console.log(computerSelection)
+        // compare result to the strings in playRound()
+        if (result === 'tie') {
+            playerScore += 1;
+            computerScore += 1;
+            console.log('tie, point to each')
+        } else if ( result === 'player'){
+            playerScore += 1;
+            console.log(`one point for player`)
+        } else if (result === 'computer') {
+            computerScore += 1;
+            console.log(`one point to computer`)
+        } else {
+            console.log('somethings wrong')
+        }
+
+    }
+    // check to see who wins
+    if (playerScore > computerScore){
+        console.log("player wins!")
+    } else if (playerScore === computerScore){
+        console.log("tie!")
+    } else {
+        console.log('computer wins!')
+    }
+}
+
+game()
